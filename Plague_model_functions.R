5cc8af54f6fd2ad9b1eba7b51f33a456c6e331f7
Lauren White <lwhite@rstudio03.research.sesync.org>
2019-05-22 15:41:09 -0400

    Added bubonic models with rat carrying capacity and resistance

--- Plague_model_functions.R
+++ Plague_model_functions.R
@@ -87,6 +87,58 @@ bubonicSEIR <- function(time, state, parameters) {
   })
 }
 
+# Bubonic SIR: Flea/Rat/Human Model with Rat Carrying Capacity & Resistance ----------------------------------------------------
+bubonicSIRratK <- function(time, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    #Rats
+    N_r  <- S_r + I_r + R_r #Total number of rats
+    dS_r <- r_r*S_r*(1-N_r/K_r)+r_r*R_r*(1-p_r)-d_r*S_r -beta_r * S_r * Fl *(1-exp(-1*alpha*N_r))/N_r #Susceptible rats
+    dI_r <- beta_r * S_r * Fl *(1-exp(-1*alpha*N_r))/N_r - gamma_r * I_r #Infected rats
+    dR_r <- r_r*R_r*(p_r-N_r/K_r)+g_r*gamma_r * I_r-d_r*R_r #Recovered rats
+    dD_r <- (1-g_r)*gamma_r * I_r #Dead rats from plague only
+    
+    #Fleas
+    dH  <- r_f*H*(1-H/K_f) #Fleas/rat
+    dFl <- (1-g_r)*gamma_r*I_r*H - d_f*Fl #Fleas in environment
+    
+    #Humans
+    N_h<- S_h + I_h + R_h #Total number of humans
+    dS_h<- -beta_h * S_h * Fl *(exp(-1*alpha*N_r))/N_h + b_h*(S_h+R_h) - d_h*S_h #susceptible humans
+    dI_h<- beta_h * S_h * Fl *(exp(-1*alpha*N_r))/N_h - gamma_h*I_h #infected humans
+    dR_h<- g_h*gamma_h * I_h- d_h*I_h #recovered humans
+    dD_h<- (1-g_h)*gamma_h * I_h #dead humans from disease
+    
+    return(list(c(dS_r, dI_r, dR_r, dD_r, dH, dFl, dS_h, dI_h, dR_h, dD_h)))
+  })
+}
+
+# Bubonic SEIR: Flea/Rat/Human Model with Rat Carrying Capacity & Resistance ----------------------------------------------------
+
+bubonicSEIRratsK <- function(time, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    #Rats
+    N_r  <- S_r + I_r + R_r #Total number of rats
+    dS_r <- r_r*S_r*(1-N_r/K_r)+r_r*R_r*(1-p_r)-d_r*S_r -beta_r * S_r * Fl *(1-exp(-1*alpha*N_r))/N_r #Susceptible rats
+    dI_r <- beta_r * S_r * Fl *(1-exp(-1*alpha*N_r))/N_r - gamma_r * I_r #Infected rats
+    dR_r <- r_r*R_r*(p_r-N_r/K_r)+g_r*gamma_r * I_r-d_r*R_r #Recovered rats
+    dD_r <- (1-g_r)*gamma_r * I_r #Dead rats from plague only
+    
+    #Fleas
+    dH  <- r_f*H*(1-H/K_f) #Fleas/rat
+    dFl <- (1-g_r)*gamma_r*I_r*H - d_f*Fl #Fleas in environment
+    
+    #Humans
+    N_h<- S_h + E_h+ I_h + R_h #Total number of humans
+    dS_h<- -beta_h * S_h * Fl *(exp(-1*alpha*N_r))/N_h + b_h*(S_h+R_h) - d_h*S_h #susceptible humans
+    dE_h<- beta_h * S_h * Fl *(exp(-1*alpha*N_r))/N_h - sigma_h*E_h #exposed humans
+    dI_h<- sigma_h*E_h - gamma_h*I_h #infected humans
+    dR_h<- g_h*gamma_h * I_h - d_h*I_h #recovered humans
+    dD_h<- (1-g_h)*gamma_h * I_h #dead humans from disease
+    
+    return(list(c(dS_r, dI_r, dR_r, dD_r, dH, dFl, dS_h, dE_h, dI_h, dR_h, dD_h)))
+  })
+}
+
 
 # Flea/Rat/Human Model + Pneumonic Plague Transmission Route ----------------------------------------------------
 
